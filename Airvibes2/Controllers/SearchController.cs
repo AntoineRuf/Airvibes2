1ead239c2aac90ed5b44857c6dc926d8049b4389
Moonvibes <antoine.ruffion@gmail.com>
2017-06-12 20:53:41 -0400

    Search functionality beginning
    Added partial views to display artists and records result.
    Added search view.
    Todo :
    -Create partial view for songs
    -Append partial view of results to the Search index view, rather than going to a new page each time

---
+++ Airvibes2/Controllers/SearchController.cs
@@ -0,0 +1,47 @@
+﻿using Airvibes2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Airvibes2.Controllers
+{
+    public class SearchController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Search
+
+        public ActionResult Index(string searchString)
+        {
+            var context = new ApplicationDbContext();
+            string searchCriteria = Request.Form["criteria"];
+            if (searchString != null)
+            {
+                if(searchCriteria == "Artiste")
+                {
+                    List<Artists> artists = db.Artists.Where(s => s.Name.Contains(searchString)).ToList();
+                    return PartialView("ArtistsResult", artists);
+                }
+                else if(searchCriteria == "Album")
+                {
+                    Records records = (Records)db.Records.Where(s => s.Title.Contains(searchString));
+                    return PartialView("RecordsResult",records);
+                }
+                else if(searchCriteria == "Titre")
+                {
+                    Songs songs = (Songs)db.Songs.Where(s => s.Title.Contains(searchString));
+                    return PartialView("SongsResult",songs);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
